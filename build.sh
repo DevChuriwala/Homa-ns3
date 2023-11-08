51760e82bba714dfae7598307a5fe88ad23c40b1
cameronmcginley <mcginleycameron@gmail.com>
2023-11-08 12:54:25 -0700

    Helper scripts

---
+++ build.sh
@@ -0,0 +1,12 @@
+# Print hi
+echo "Running ./waf configure"
+sleep 0.3
+./waf configure
+
+echo "\nRunning ./waf"
+sleep 0.3
+./waf
+
+echo "\nRunning simple-test"
+sleep 0.3
+./waf --run scratch/HomaL4Protocol-simple-test
---
+++ run_repro.sh
@@ -0,0 +1,3 @@
+echo "Running ./waf configure"
+sleep 0.5
+./waf --run scratch/HomaL4Protocol-simple-topology
