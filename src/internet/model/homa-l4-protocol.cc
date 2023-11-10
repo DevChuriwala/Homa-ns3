fefc0cce9510722d27380995bbad4faa698cc572
Abhishek Mysore Harish <amysoreharish@ucsd.edu>
2023-11-10 12:54:58 -0800

    Added logs

--- src/internet/model/homa-l4-protocol.cc
+++ src/internet/model/homa-l4-protocol.cc
@@ -173,8 +173,10 @@ HomaL4Protocol::GetBdpFromPort(uint16_t sport, uint16_t dport) const
   if (((sport == 1000 || sport == 1001 || sport == 1002 || sport == 1003) && 
     (dport == 1000 || dport == 1001 || dport == 1002 || dport == 1003)) || ((sport == 1004 || sport == 1005 || sport == 1006 || sport == 1007) && 
     (dport == 1004 || dport == 1005 || dport == 1006 || dport == 1007))) {
+    NS_LOG_WARN("GetBdpFromPort 10" << sport << " " << dport);
     return 10;
   } else {
+    NS_LOG_WARN("GetBdpFromPort 20" << sport << " " << dport);
     return 20;
   }
 }
@@ -187,8 +189,10 @@ HomaL4Protocol::GetBdpFromIP(uint32_t saddr, uint32_t daddr) const
 
   // Check if in same switch (both <=3 or >=4)
   if ((saddrThirdOctet <= 3 && daddrThirdOctet <= 3) || (saddrThirdOctet >= 4 && daddrThirdOctet >= 4)) {
+    NS_LOG_WARN("GetBdpFromIP 10" << saddr << " " << daddr);
     return 10;
    } else {
+    NS_LOG_WARN("GetBdpFromIP 20" << saddr << " " << daddr);
     return 20;
   }
 }
